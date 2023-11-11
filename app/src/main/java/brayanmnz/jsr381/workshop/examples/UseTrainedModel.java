310d18560d4eeb717d4c81a68168fe28addc9943
Brayan M <brayanmnz@hotmail.com>
2023-11-11 12:28:05 -0400

    Updating project to run with JDK21 and load correct files

--- app/src/main/java/brayanmnz/jsr381/workshop/examples/UseTrainedModel.java
+++ app/src/main/java/brayanmnz/jsr381/workshop/examples/UseTrainedModel.java
@@ -9,22 +9,26 @@ import javax.visrec.ri.ml.classification.ImageClassifierNetwork;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class UseTrainedModel {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         // load a trained model/neural network
-        ConvolutionalNetwork convNet = FileIO.createFromFile("hotdog.dnet", ConvolutionalNetwork.class);
+        ConvolutionalNetwork convNet = FileIO.createFromFile("../hotdog.dnet", ConvolutionalNetwork.class);
         // create an image classifier using trained model
         ImageClassifier<BufferedImage> classifier = new ImageClassifierNetwork(convNet);
 
         // load image to classify
-        BufferedImage image = ImageIO.read(new File("app/src/main/resources/hot_dog.jpg"));
+        //BufferedImage image = ImageIO.read(new File("./src/main/resources/hot_dog.jpg"));
+
+        BufferedImage image = ImageIO.read(new File("./src/main/resources/pizza.jpg"));
         // feed image into a classifier to recognize it
         Map<String, Float> results = classifier.classify(image);
 
-        System.out.println(results);
+        System.out.println(results + "\n");
 
         // interpret the classification result / class probability
         float hotDogProbability = results.get("hot_dog");
